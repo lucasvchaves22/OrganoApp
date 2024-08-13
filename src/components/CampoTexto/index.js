816ffdc3dde7c9a4a1565beff31538b9e7364fee
Lucas Vasconcelos Chaves- (Infra/Sec - CM) <lucas.chaves@consorcioluiza.intranet>
2024-08-13 17:06:18 -0300

    Manipulando os valores dos inputs

--- src/components/CampoTexto/index.js
+++ src/components/CampoTexto/index.js
@@ -4,10 +4,19 @@ const CampoTexto = (props) => {
 
     const placeholderModificada = `${props.placeholder}...`;
 
+    let valor = 'Lucas Vasconcelos'
+    
+    const aoDigitado = (evento) => {
+        valor = evento.target.value
+        console.log(valor)
+    }
+
     return (
         <div className="campo-texto">
-            <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={placeholderModificada}/>
+            <label>
+                {props.label}
+            </label>
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
         </div>
     )
 }
