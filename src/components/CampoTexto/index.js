dd159cb423b4b2b893d4c7de654a46fd2db42464
Lucas Vasconcelos Chaves- (Infra/Sec - CM) <lucas.chaves@consorcioluiza.intranet>
2024-08-13 17:37:18 -0300

    controlando o estado dos inputs

--- src/components/CampoTexto/index.js
+++ src/components/CampoTexto/index.js
@@ -3,12 +3,9 @@ import './CampoTexto.css'
 const CampoTexto = (props) => {
 
     const placeholderModificada = `${props.placeholder}...`;
-
-    let valor = 'Lucas Vasconcelos'
     
     const aoDigitado = (evento) => {
-        valor = evento.target.value
-        console.log(valor)
+        props.aoAlterado(evento.target.value)
     }
 
     return (
@@ -16,7 +13,7 @@ const CampoTexto = (props) => {
             <label>
                 {props.label}
             </label>
-            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
         </div>
     )
 }
