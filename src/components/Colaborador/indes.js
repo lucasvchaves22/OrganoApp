a8c51d0525bb70a06fe1b62e35b497f08affd52d
Lucas Vasconcelos Chaves- (Infra/Sec - CM) <lucas.chaves@consorcioluiza.intranet>
2024-08-16 10:29:53 -0300

    Adicionando o card e estilo dos colaboradores

---
+++ src/components/Colaborador/indes.js
@@ -0,0 +1,16 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return(<div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/lucasvchaves22.png' alt='Lucas Vasconcelos'></img>
+            </div>
+            <div className='rodape'>
+                <h4>Lucas Vasconcelos</h4>
+                <h5>Analista</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
