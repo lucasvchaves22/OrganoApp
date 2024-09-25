0af5d64dad86004533aadd05308cc86f79ad96ee
Lucas Vasconcelos <lucasvchaves22@gmail.com>
2024-09-25 19:44:57 -0300

    Finalizando Projeto

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,10 +1,15 @@
+import { Fragment } from 'react'
 import './Banner.css'
 
 function Banner() {
+    // JSX
     return (
-    <header className="banner">
-        <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
-    </header>    
+        // <fragment></fragment>
+        <>   
+            <header className="banner">
+                <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
+            </header>
+        </>    
     )
 
 }
